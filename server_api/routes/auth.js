90fb15fcd741b45dcffae81036aa0068dcd26146
sandeep <sandeepsanu1230@gmail.com>
2024-08-04 20:27:54 +0530

    Fix bcrypt compatibility issue

--- server_api/routes/auth.js
+++ server_api/routes/auth.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const User = require('../models/user');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const multer = require('multer');
 const upload = multer();
 
--- server_api/scripts/admin.js
+++ server_api/scripts/admin.js
@@ -1,6 +1,6 @@
 const express=require("express")
 const User=require("../models/user")
-const bcrypt=require("bcrypt")
+const bcrypt=require("bcryptjs")
 
 
 async function createAdminAccount(){
